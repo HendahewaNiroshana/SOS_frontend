d0e846893c9c368f7e3fa0505a3d4eb21c67a534
Hendahewa Mishad Vihanga Niroshana <10953093@students.plymouth.ac.uk>
2026-03-21 13:40:40 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-//const BACKEND_URL = "https://your-sos-game-backend.onrender.com";
-const BACKEND_URL = "http://localhost:5000"; 
+const BACKEND_URL = "sosbackend-production.up.railway.app";
+//const BACKEND_URL = "http://localhost:5000"; 
 
 export const API_BASE_URL = BACKEND_URL;
 export const SOCKET_URL = BACKEND_URL;
