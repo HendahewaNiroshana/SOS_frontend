016f1e0513079b39ff4b1288c4cc9d4d8e4f96e4
Hendahewa Mishad Vihanga Niroshana <10953093@students.plymouth.ac.uk>
2026-03-21 13:43:58 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const BACKEND_URL = "sosbackend-production.up.railway.app";
-//const BACKEND_URL = "http://localhost:5000"; 
+
 
 export const API_BASE_URL = BACKEND_URL;
 export const SOCKET_URL = BACKEND_URL;
